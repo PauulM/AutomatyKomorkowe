1dac132d72abe83eec9dff134efbf2ee5f9fb1fa
pawma <pawmat@student.agh.edu.pl>
2016-12-07 20:28:48 +0100

    6712 20:28

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,87 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.util.Map;
 
 public class Controller {
+
+    private Stage stage;
+    private Scene scene;
+    private Automaton automaton;
+    private int width;
+    private int height;
+    private int radius;
+    private CellStateFactory factory;
+    private Map<CellCoordinates, CellState> map;
+    private CellNeighborhood neighborsStrategy;
+    private GameOfLifeRules golRules;
+    private boolean wrapping = false;
+
+    public void setStage(Stage stage){
+        this.stage = stage;
+    }
+
+    public void setScene(Scene scene){
+        this.scene = scene;
+    }
+
+    public void gameOfLifeButton(){
+
+        //automaton = new GameOfLife(this.width, this.height, )
+
+        //1.tworzy factory
+        //2. tworzy automaton
+        //3. insert structure
+    }
+
+    public void submitGameOfLifeRulesButton(){
+        this.golRules = new GameOfLifeRules(getTextFromTextField("#remainsAliveField"),
+                getTextFromTextField("#bornsField"));
+    }
+
+    private String getTextFromTextField(String fieldId){
+        return (((TextField)scene.lookup(fieldId)).getText());
+    }
+
+    public void submitButton(){
+        this.width = Integer.parseInt(getTextFromTextField("#widthField"));
+        this.height = Integer.parseInt(getTextFromTextField("#heightField"));
+        this.radius = Integer.parseInt(getTextFromTextField("#radiusField"));
+    }
+
+    public void moorNeighborhoodButton(){
+        this.neighborsStrategy = new MoorNeighborhood(this.width, this.height, this.wrapping, this.radius);
+    }
+
+    public void wrappingButton(){
+        if(this.wrapping == false)
+            this.wrapping = true;
+        else
+            this.wrapping = false;
+    }
+
+    public void initializeBoardButton(){
+        //tworzy planszę na której można wybrać komórki startowe
+        
+        HBox boardHbox = (HBox) scene.lookup("#boardHbox");
+        Button button = new Button("abc");
+        boardHbox.getChildren().addAll(button);
+    }
+
+    public void acceptStartingBoardButton(){
+        //akceptuje wybrane komórki startowe
+        //inicjalizuje map
+
+    }
+
+    public void printIfClicked(){
+        System.out.println("click");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,8 @@ import static javafx.application.Application.launch;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 
 public class Main extends Application{
 
@@ -18,9 +13,14 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUIMenu.fxml"));
+        Parent root = (Parent)fxmlLoader.load();
+        Controller controller = (Controller)fxmlLoader.getController();
+        controller.setStage(primaryStage);
         primaryStage.setTitle("Cellular automatons");
-        primaryStage.setScene(new Scene(root, 1000, 600));
+        Scene scene = new Scene(root, 1000, 600);
+        controller.setScene(scene);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
 
